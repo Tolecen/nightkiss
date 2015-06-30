9f099633c23f858819ad1e81ea977c368dba460f
Tolecen <taox.l@163.com>
2015-06-30 19:13:51 +0800

    Initial Commit

---
+++ NightKiss/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NightKiss
+//
+//  Created by Tolecen on 15/6/30.
+//  Copyright (c) 2015年 Tolecen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
